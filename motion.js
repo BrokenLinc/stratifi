af0c04da25627a8aac60a1f0dbc2f454f62ecfbe
Eran Schoellhorn <eschoellhorn@352inc.com>
2016-09-09 14:12:20 -0400

    Merge codebases

---
+++ motion.js
@@ -0,0 +1,56 @@
+(function(){
+
+    var $motion = $('#motion');
+    var $orientation = $('#orientation');
+    var $calibrate = $('#calibrate');
+    var $notes = $('#notes');
+
+    var device = {
+      calibrationPoint: 0,
+      calibrationAccell: 0,
+      direction: 0,
+      accell: 0
+    };
+
+    var aBuffer = [];
+
+    function motion(e) {
+      var c = event.accelerationIncludingGravity;
+      var fun = _.flow(Math.sqrt, v => v * 10, Math.round)
+      device.accell = fun([c.x, c.y, c.z]
+        .map(val => Math.pow(val, 2))
+        .reduce((carry, num) => carry + num, 0));
+      var adjustedAccell = device.calibrationAccell - device.accell;
+      aBuffer.push(adjustedAccell);
+      if (aBuffer.length > 6) {
+        aBuffer.shift();
+        var buffer = aBuffer
+          .concat()
+          .sort();
+        $motion.text(buffer.join(', '));
+      }
+    }
+
+    function orientation(e) {
+      device.direction = Math.ceil((e.alpha * .009) || 0);
+      var adjustedDirection = device.adjustedDirection = device.direction - device.calibrationPoint;
+      $notes.removeClass().addClass(adjustedDirection);
+      addItem($orientation, `<li>${adjustedDirection}</li>`);
+    }
+
+    window.addEventListener('devicemotion', motion, false);
+    window.addEventListener('deviceorientation', orientation, false);
+
+    function addItem(list, value) {
+      list.html($(value));
+    }
+
+    $calibrate.on('click', calibrateDirection);
+
+    function calibrateDirection() {
+      $motion.text('');
+      device.calibrationPoint = device.direction;
+      device.calibrationAccell = device.accell;
+    }
+
+  })();
